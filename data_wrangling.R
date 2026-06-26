fbaaff90742f9b111157aee6a2dde12441a71092
northpaule <pmathieu003@gmail.com>
2026-06-26 12:10:01 +1000

    data cleaning

--- data_wrangling.R
+++ data_wrangling.R
@@ -3,3 +3,11 @@
 # loading data
 library(readxl)
 watermans <- read_excel("data/lsw_watermans.xlsx")
+
+# removing columns to ignore
+clean_watermans <- watermans %>%
+        select(-c('Botrylloides_back', 'Styela_back', 'note', 'Diver_ID'))
+
+# rounding styela dry weight to 1 decimal place
+clean_watermans <- clean_watermans %>%
+        mutate(Styela_dry_weight = round(as.numeric(Styela_dry_weight), 1))
