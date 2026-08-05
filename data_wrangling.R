761547ffdf99e2bfbf01920a8e0ad4e4f7464f6c
northpaule <pmathieu003@gmail.com>
2026-08-05 11:12:09 +1000

    standardized botrylloides size classes

--- data_wrangling.R
+++ data_wrangling.R
@@ -6,8 +6,53 @@ watermans <- read_excel("data/lsw_watermans.xlsx")
 
 # removing columns to ignore
 clean_watermans <- watermans %>%
-        select(-c('Botrylloides_back', 'Styela_back', 'note', 'Diver_ID'))
+        select(-c('Kelp_transplant', 'Botrylloides_back', 'Styela_front', 'Styela_back', 'note', 'Diver_ID'))
 
 # rounding styela dry weight to 1 decimal place
 clean_watermans <- clean_watermans %>%
         mutate(Styela_dry_weight = round(as.numeric(Styela_dry_weight), 1))
+
+# clean Botrylloides 
+        #desired size classes: 0, XS, S, M, L, XL, XXL, NA
+clean_watermans <- clean_watermans %>%
+        mutate(Botrylloides_front = as.character(Botrylloides_front),
+                Botrylloides_front = case_when(
+                Botrylloides_front %in% c("XXXL", "XXXXL") ~ "XXL", #collapse the largest size classes
+                TRUE ~ Botrylloides_front),
+                Botrylloides_front = factor(
+                        Botrylloides_front, levels = c("0","XS","S","M","L","XL","XXL"), #convert back to an ordered factor
+                        ordered = TRUE))
+
+
+# convert variables into usable factors
+clean_watermans <- clean_watermans %>%
+        mutate(Side = factor(Side),
+                Panel_number = factor(Panel_number),
+                Depth = factor(Depth,
+                        levels = c("Intertidal","1.2m","3m")),
+                Panel_front = factor(Panel_front),
+                Sampling_period = factor(
+                        Sampling_period,
+                        levels = c("S1","S2","S3","S4"),
+                        ordered = TRUE),
+               Span = factor(Span),
+               Date = as.Date(Date, format = "%d/%m/%Y"),
+               Styela_actual = as.numeric(Styela_actual))
+
+str(clean_watermans) # check
+
+# how many panel designs
+levels(clean_watermans$Panel_front)
+        ## 6:Control, Kelp, Oyster, Rockpool, Sponge, Texture
+
+# create unique identifier for each individual panel
+clean_watermans <- clean_watermans %>%
+        mutate(Panel_ID = interaction(
+                        Span,
+                        Depth,
+                        Panel_number,
+                        drop = TRUE))
+
+
+
+
